6ce394632434e5f2b0c0f18c0fabec8ad66f35c2
uetiari <arianeueti@gmail.com>
2019-10-05 16:40:42 -0300

    Adicionado folha de script

---
+++ script.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        titulo: "Teste PushStart - Login",
+        login: "",
+        password: "",
+        btnSubmit: "btn__submit"
+    }
+    methods: {
+        
+    }
+});
