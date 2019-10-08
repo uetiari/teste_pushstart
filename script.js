2b7667e3520b063cd80b2c7061b0be4c626ac57b
Ariane Ueti <42356440+uetiari@users.noreply.github.com>
2019-10-08 16:40:01 -0300

    Ajuste no botão Entrar

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
-
 Vue.component('top-bar', {
     template: "#feedback__topbar"
 });
 
+var config = {
+    headers: {'X-Auth-Token': 'rKlEuxcYJT'},
+    crossDomain: true,
+};
+
+var bodyParameters = {
+   username: "ash",
+   password: "butterfree"
+}
+
+axios.get('https://teste.pushstart.com.br/api/', { 
+    params:{
+    }, 
+    headers: { 'X-Auth-Token': 'rKlEuxcYJT' },
+    'Access-Control-Allow-Origin': '*',
+    'Accept': 'application/json',
+    'Content-Type': 'application/json',
+});
+
+
+axios.post( 
+  'https://teste.pushstart.com.br/api/login',
+  bodyParameters,
+  config,
+).then((response) => {
+//   console.log(response)
+}).catch((error) => {
+//   console.log(error)
+});
+
+
 var app = new Vue({
     el: "#app",
     data: {
-        login: "",
-        password: "",
+        login: '',
+        password: '',
         btnSignIn: "btn__signin",
         btnFeedback: "btn__feedback"
     },
